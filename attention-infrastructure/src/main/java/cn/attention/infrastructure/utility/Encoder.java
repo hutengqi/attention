fa85dd3c59dea8cc98945442e6c2f59ec25b55bd
Ht7 <18838292213@163.com>
2022-11-03 22:09:49 +0800

    first push.

---
+++ attention-infrastructure/src/main/java/cn/attention/infrastructure/utility/Encoder.java
@@ -0,0 +1,11 @@
+package cn.attention.infrastructure.utility;
+
+/**
+ * 加密工具类
+ *
+ * @author Ht7_Sincerity
+ * @date 2022/11/3
+ */
+public class Encoder {
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Attention
+
+基于 Spring Cloud Alibaba 的轮子
